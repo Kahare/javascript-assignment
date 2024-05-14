eff9acb7942e16bc6c2c29957e1e49fe8ace4654
Kahare <tony.kahare@gmail.com>
2024-05-14 17:26:16 +0300

    Updated file

--- generator.js
+++ generator.js
@@ -1,10 +1,20 @@
-function generatePassword() {
-    var length = 10; // length of the password
-    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+{}[]|;:,.<>?";
-    var retVal = "";
-    for (var i = 0, n = charset.length; i < length; i++) {
-       retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
-  }
-  console.log(generatePassword());
+function generatePassword(firstName, lastName) {
+   // Check if the input names are strings
+   if (typeof firstName !== 'string' || typeof lastName !== 'string') {
+       throw new Error('Both firstName and lastName must be strings');
+   }
+
+   // Get the first 3 characters of the first name and the last 3 characters of the last name
+   const firstThreeChars = firstName.substring(0, 3);
+   const lastThreeChars = lastName.substring(lastName.length - 3, lastName.length);
+
+   // Combine the characters and add "@2024" to the end
+   const generatedPassword = firstThreeChars + lastThreeChars + '@2024';
+
+   return generatedPassword;
+}
+
+// Example usage
+const userName = 'Anthony Kahare';
+const password = generatePassword(...userName.split(' '));
+console.log('Generated Password:', password);
