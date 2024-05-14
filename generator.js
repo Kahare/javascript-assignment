292bad99a1ff05794c2178854c213fbc16e15839
Kahare <tony.kahare@gmail.com>
2024-05-14 17:33:19 +0300

    Javascript file

--- generator.js
+++ generator.js
@@ -4,17 +4,15 @@ function generatePassword(firstName, lastName) {
        throw new Error('Both firstName and lastName must be strings');
    }
 
-   // Get the first 3 characters of the first name and the last 3 characters of the last name
+
    const firstThreeChars = firstName.substring(0, 3);
    const lastThreeChars = lastName.substring(lastName.length - 3, lastName.length);
 
-   // Combine the characters and add "@2024" to the end
    const generatedPassword = firstThreeChars + lastThreeChars + '@2024';
 
    return generatedPassword;
 }
 
-// Example usage
 const userName = 'Anthony Kahare';
 const password = generatePassword(...userName.split(' '));
 console.log('Generated Password:', password);
